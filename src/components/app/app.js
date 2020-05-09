66f34115d468a25ad2e17241fb0e99743294d176
kosmoden4ik <kosmoden4ik@gmail.com>
2020-05-09 20:51:38 +0300

    make structure

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,26 @@
 import React from "react";
 import "./app.css";
+import ItemList from "../item-list.js";
+import Header from "../header.js";
+import PlanetDetails from "../planet-details.js";
+import RandomPlanet from "../random-planet.js";
+import PersonDetails from "../person-details.js";
+import StarshipDetails from "../starship-details.js";
+import SwapiService from ".../swapi-services.js";
 const App = () => {
-  return <div className=""></div>;
+  return (
+    <div>
+      <Header />
+      <RandomPlanet />
+      <div className="row mb2">
+        <div className="col-md-6">
+          <ItemList />
+        </div>
+        <div className="col-md-6">
+          <PersonDetails />
+        </div>
+      </div>
+    </div>
+  );
 };
 export default App;
