b219021ee45625539d90af32cf38ee9c7f378bae
kosmoden4ik <kosmoden4ik@gmail.com>
2020-05-22 21:56:03 +0300

    refactor spinner

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -22,37 +22,43 @@ export default class RandomPlanet extends Component {
     this.swapiService.getPlanet(id).then(this.onPlanetLoaded);
   }
   render() {
-    const {
-      planet: { id, name, population, rotationPeriod, diameter },
-      loading,
-    } = this.state;
-    if (loading) {
-      return <Spinner />;
-    }
+    const { planet: planet, loading } = this.state;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !loading ? <PlanetView planet={planet} /> : null;
     return (
       <div className="random-planet jumbotron rounded">
-        <img
-          className="planet-images jambotron rounded"
-          src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
-        />
-        <div className="card-body text-white ">
-          <h4>{name} </h4>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Population </span>
-              <span>{population}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Rotation Period </span>
-              <span>{rotationPeriod}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Diameter </span>
-              <span>{diameter}</span>
-            </li>
-          </ul>
-        </div>
+        {spinner}
+        {content}
       </div>
     );
   }
 }
+
+const PlanetView = ({ planet }) => {
+  const { id, name, population, rotationPeriod, diameter } = planet;
+  return (
+    <React.Fragment>
+      <img
+        className="planet-images jambotron rounded"
+        src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+      />
+      <div className="card-body text-white ">
+        <h4>{name} </h4>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <span className="term">Population </span>
+            <span>{population}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Rotation Period </span>
+            <span>{rotationPeriod}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Diameter </span>
+            <span>{diameter}</span>
+          </li>
+        </ul>
+      </div>
+    </React.Fragment>
+  );
+};
